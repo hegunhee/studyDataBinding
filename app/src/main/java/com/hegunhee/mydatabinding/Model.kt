97057ea6efde5fc43fca43b7e335f28d3e978a61
hegunhee <gunhee0072@gmail.com>
2022-03-21 21:10:59 +0900

    Int type도 databinding에 사용될 수 있을지 테스트해봤습니다.

--- app/src/main/java/com/hegunhee/mydatabinding/Model.kt
+++ app/src/main/java/com/hegunhee/mydatabinding/Model.kt
@@ -6,4 +6,13 @@ import androidx.lifecycle.MutableLiveData
 data class Model(var first_name : String, val second_name : String) {
     val text = MutableLiveData<String>("default")
 
+    var intValue = MutableLiveData<Int>(0)
+
+
+    fun onClick(){
+        intValue.value = intValue.value!!+1
+        Log.d("ClickTest","click${intValue}")
+
+    }
+
 }
