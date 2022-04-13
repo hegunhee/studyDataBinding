11d145ae470f8d6672b56567e16c349caf9c588f
hegunhee <gunhee0072@gmail.com>
2022-04-13 17:07:47 +0900

    기타 코드 개선

--- app/src/main/java/com/hegunhee/mydatabinding/RecyclerViewHolder.kt
+++ app/src/main/java/com/hegunhee/mydatabinding/RecyclerViewHolder.kt
@@ -11,17 +11,18 @@ import com.hegunhee.mydatabinding.databinding.ItemBinding
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>() {
     var movieList = ArrayList<Movie>()
 
-    inner class RecyclerViewHolder(private val binding : ItemBinding) : RecyclerView.ViewHolder(binding.root){
+    inner class RecyclerViewHolder(private val binding: ItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(movie: Movie) {
-            binding.title.text = movie.title
-            binding.content.text = movie.context
+        fun bind(movie: Movie) = with(binding) {
+            title.text = movie.title
+            content.text = movie.context
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
-        val binding = ItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = ItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return RecyclerViewHolder(binding)
     }
 
@@ -32,14 +33,15 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>
     override fun getItemCount(): Int {
         return movieList.size
     }
-    fun setItems(items: ArrayList<Movie>){
+
+    fun setItems(items: ArrayList<Movie>) {
         movieList = items
         notifyDataSetChanged()
     }
 }
 
 @BindingAdapter("bind:movieList")
-fun setItems(recyclerView: RecyclerView, items : ArrayList<Movie>){
+fun setItems(recyclerView: RecyclerView, items: ArrayList<Movie>) {
     (recyclerView.adapter as RecyclerAdapter)?.run {
         setItems(items)
     }
